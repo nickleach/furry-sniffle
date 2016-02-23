8017afd47b2484e512803cc045b38cee1f3f9803
Isaac Hardy <isaachardyDEV@gmail.com>
2016-02-23 16:45:58 -0500

    Reverted back to previous commit

--- components/charts/summaryChart.js
+++ components/charts/summaryChart.js
@@ -1,6 +1,5 @@
 import React, { StyleSheet, View, Text, WebView} from 'react-native';
 import * as ChartUtl from './chart.utl.js';
-import Highcharts from 'highcharts';
 
 const
     _component = React.createClass({
@@ -32,98 +31,12 @@ const
             };
         },
         componentWillMount() {
-          // Highcharts.Chart('sum-chart', {
-          //     chart: {
-          //         type: 'bar',
-          //         renderTo: 'sum-chart',
-          //         reflow: true,
-          //         height: this.state.height,
-          //         width: this.state.width,
-          //         style: {
-          //             fontFamily: 'Arial Narrow, Arial, sans serif'
-          //         }
-          //     },
-          //     exporting: {
-          //         enabled: false,
-          //     },
-          //     title: {
-          //         text: this.state.chartTitle[0],
-          //         align: 'left',
-          //         style: {
-          //             fontSize: '1.15em',
-          //             fontWeight: 'bold'
-          //         }
-          //     },
-          //     xAxis: {
-          //         gridLineColor: null,
-          //         lineColor: null,
-          //         categories: this.state.cat,
-          //         tickmarkPlacement: null,
-          //         tickLength: 0,
-          //         labels: {
-          //             style: {
-          //                 fontSize: '0.875em',
-          //                 fontWeight: 'bold',
-          //                 textOverflow: 'none'
-          //             }
-          //         }
-          //     },
-          //     credits: {
-          //         enabled: false
-          //     },
-          //     plotOptions: {
-          //         series: {
-          //             colorByPoint: true
-          //         },
-          //         bar: {
-          //             dataLabels: {
-          //                 enabled: true,
-          //                 align: 'left',
-          //                 inside: true,
-          //                 color: 'white',
-          //                 style: {
-          //                     fontSize: '1em',
-          //                     textShadow: null
-          //                 }
-          //             }
-          //         }
-          //     },
-          //     yAxis: {
-          //         gridLineColor: null,
-          //         plotBands: [{
-          //             color: 'rgba(255, 204, 0, 0.67)',
-          //             from: 75,
-          //             to: 90,
-          //             zIndex: 5
-          //         },
-          //             {
-          //                 color: 'rgba(115, 208, 127, 0.67)',
-          //                 from: 90,
-          //                 to: 100,
-          //                 zIndex: 5
-          //             }],
-          //         labels: {
-          //             enabled: false
-          //         },
-          //         title: {
-          //             enabled: false
-          //         }
-          //     },
-          //     series: [{
-          //         showInLegend: false,
-          //         data: [this.state.randomSet[0], this.state.randomSet[1], this.state.randomSet[2], this.state.randomSet[3], this.state.randomSet[4]],
-          //         colors: [this.state.aggregateCol, this.state.projectCol, this.state.projectCol, this.state.projectCol, this.state.nationalCol],
-          //         groupPadding: 0,
-          //         pointPadding: 0.07,
-          //         borderColor: null
-          //     }]
-          // });
+
         },
         render() {
             return(
                 <View>
-                    <Text>{this.state.randomSet}</Text>
-                    
+                    <Text>{this.state.height}</Text>                    
                 </View>
             );
         }

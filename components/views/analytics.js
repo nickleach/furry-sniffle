3d00d8bf3c2eb6f082c1141be1dd9c072293dcb2
Cori Fowler <fowler.cori@gmail.com>
2016-02-23 14:36:44 -0500

    added tabs to analytics page

--- components/views/analytics.js
+++ components/views/analytics.js
@@ -1,7 +1,21 @@
-import React, {StyleSheet, Text, View} from 'react-native';
+import React, {StyleSheet, Text, View, TabBarIOS} from 'react-native';
 
 const
     styles = StyleSheet.create({
+        tabButton: {
+            borderColor: 'white',
+            borderRadius: 3,
+            borderWidth: 1,
+            borderStyle: 'solid'            
+        },
+        tabContent: {
+            flex: 1,
+            alignItems: 'center',
+        },
+        tabText: {
+            color: 'white',
+            margin: 50
+        },
         container: {
             flex: 1,
             justifyContent: 'center',
@@ -10,11 +24,68 @@ const
       }
     }),
     _component =  React.createClass({
+
+        statics: {
+            title: 'Analytics',
+            description: 'Analytics Tabs'
+        },
+
+        displayName: 'Analytics',
+
+        getInitialState() {
+            return {
+                selectedTab: 'analyticsTab'
+            };
+        },
+
+        _renderContent(color, pageText) {
+            return(
+                <View style={[styles.tabContent, {backgroundColor: color}]}>
+                    <Text style={styles.tabText}>{pageText}</Text>
+                </View>
+            );
+        },
+
         render() {
             return (
-                <View style={styles.container}>
-                    <Text> Analytics Page </Text>
-                </View>
+                <TabBarIOS
+                    tintColor="white"
+                    barTintColor="#296cdc">
+                    <TabBarIOS.Item
+                        title="Analytics"
+                        style={styles.tabButton}
+                        selected={this.state.selectedTab === 'analyticsTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'analyticsTab',
+                            });
+                        }}>
+                        {this._renderContent('#414A8C', 'Analytics Page')}
+                    </TabBarIOS.Item>
+                    <TabBarIOS.Item
+                        title="Disposition Profile"
+                        selected={this.state.selectedTab === 'dispositionTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'dispositionTab',
+                            });
+                        }}>
+                        {this._renderContent('#e4b600', 'Disposition Profile Page')}
+                    </TabBarIOS.Item>
+                    <TabBarIOS.Item
+                        title="Demographic Profile"
+                        selected={this.state.selectedTab === 'demographicTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'demographicTab',
+                            });
+                        }}>
+                        {this._renderContent('#ff9d00', 'Demographic Profile Page')}
+                    </TabBarIOS.Item>
+                </TabBarIOS>
+                // <View style={styles.container}>
+                //     <Text> Analytics Page </Text>
+                // </View>
             );
         }
     });
